ec824ffd8a679ac1bb9a03fde37e3493fda83824
galaxyfreak <marinspajic17@hotmail.com>
2014-12-30 10:27:12 +0100

    Android.mk: Symlink WCD9306 firmware
    Change-Id: I70422369cd6c703ace900b8599707f3f8e170dd1

--- Android.mk
+++ Android.mk
@@ -18,4 +18,10 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),eagle)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+    $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9306; \
+        ln -sf /data/misc/audio/wcd9320_anc.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_anc.bin; \
+        ln -sf /data/misc/audio/mbhc.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_mbhc.bin)
 endif
