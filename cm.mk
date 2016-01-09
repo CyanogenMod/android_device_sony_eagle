b5e1d00d7070036624c5b29da28b51475f6c2345
Anik khan <anik9280@gmail.com>
2016-01-09 21:30:36 +0600

    eagle: Initial CM Bringup
    Change-Id: I9f76bf4699c899e27a1856545f62411609b405ca
    Signed-off-by: Anik khan <anik9280@gmail.com>

---
+++ cm.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Resolution
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := D2303,eagle
+
+# Inherit from eagle device
+$(call inherit-product, device/sony/eagle/aosp_d2303.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit CM common stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    device/sony/eagle/bootrec-device:root/sbin/bootrec-device
+
+# FingerPrint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=D2303 \
+    BUILD_FINGERPRINT=Sony/D2303/D2303:4.4.4/18.3.1.C.0.21/2n9_bg:user/release-keys \
+    PRIVATE_BUILD_DESC="D2303-user 18.3.1.C.0.21 2n9_bg release-keys"
+
+PRODUCT_NAME := cm_eagle
+PRODUCT_DEVICE := eagle
+PRODUCT_MODEL := Xperia M2
